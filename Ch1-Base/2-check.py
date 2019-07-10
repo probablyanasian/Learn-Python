6ed996d7c15c6d0e8dd507bc6ae33783d3b3bea7
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-07-10 06:51:42 -0700

    Fixed 1.2, front vs back

--- Ch1-Base/2-check.py
+++ Ch1-Base/2-check.py
@@ -20,7 +20,7 @@ num_of_let = int(check_type)
 
 while True:
   check_type = input("Front or back: ").lower()
-  if check_type in ['front', 'back']:
+  if check_type in ['front', 'back', 'f', 'b']:
     break
   else:
     print('Input has to be either \'front\' or \'back\'')
@@ -34,13 +34,18 @@ p.wait()
 def text_gen(letters):
   return (''.join(secrets.choice(string.ascii_letters) for _ in range(letters)))
 
-if check_type == 'front':
+if check_type in ['front', 'f']:
   start_num = num_of_let
   end_num = 20
-elif check_type == 'back':
+  expected = 'start_text'
+
+elif check_type in ['back', 'b']:
   start_num = 20
   end_num = num_of_let
-
+  expected = 'ending_text'
+else:
+  print('Error, please report this `2-check.py broke`')
+  sys.exit()
 
 correct = True
 for _ in range(10): 
@@ -49,12 +54,13 @@ for _ in range(10):
   ending_text = text_gen(secrets.randbelow(end_num))
   input_text = start_text + ending_text
   check = p.communicate(input=input_text)[0].replace(query, '').strip()
-  if check == start_text:
+  if check == eval(expected):
     pass
   else: 
     correct = False
-    print('Input: {0}'.format(input_text))
-    print('Outputted: {0}'.format(check))
+    print(f'Input: {input_text}')
+    print(f'Outputted: {check}')
+    print(f'Expected: {eval(expected)}')
     break
   p.wait()
 
--- Ch1-Base/2-practice.py
+++ Ch1-Base/2-practice.py
@@ -4,3 +4,6 @@
 
 # Use input(), slice(), and print(). optional(list())
 # Check by running 2-check.py
+
+a = input("blah")
+print(a[:5])
