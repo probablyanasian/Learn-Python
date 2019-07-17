5153d89c5a67e2b4839419dbbb497fdd6aca127e
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-07-17 01:02:23 -0700

    4-practice decided

---
+++ Ch1-Base/4-practice.py
@@ -0,0 +1,3 @@
+# Try taking in 2 numbers seperately, then compare the two.
+# Print back whether the 2nd number is 'greater than', 'less than', or 'equal' to the first.
+# eg. Inputs: 11 3    Returns: less than
