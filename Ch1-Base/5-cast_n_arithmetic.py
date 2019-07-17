6010dd47a5baa4df00aea077b9b6c4803f98532d
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-07-17 07:58:44 -0700

    Fin 1.5 lesson.

---
+++ Ch1-Base/5-cast_n_arithmetic.py
@@ -0,0 +1,35 @@
+# Casts are conversion between types
+# Arithmetic allows you to do simple math between two numbers or variables of the right type
+
+# I'll teach two cast types here
+
+# int()  ; converts a string to an integer, however the string must be all 'number' characters. ## Specific wording for later
+# Otherwise, you'll raise a ValueError
+int('50')
+
+# str()  ; converts an object into a string
+str(300)
+
+# Arithmetic here
+# Addition
+4 + 6  # 10
+'ab' + 'cd'  # 'abcd'
+
+# Subtraction
+4 - 6  # -2
+
+# Multiplication
+4 * 6  # 24
+'ab' * 4  # 'abababab'
+
+# Division
+4 / 6  # 0.666666..
+
+# Modulo ; Remainder left after division
+6 % 4 # 2
+
+# Exponential
+4 ** 6 # 4096
+
+# Floor division ; takes off decimals after remainder
+10 // 4 # 2
